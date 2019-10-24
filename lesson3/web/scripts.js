4ceba5da2989d4da7c39c171e25ecdce12875a62
Riazantseva <48712135+Riazantseva@users.noreply.github.com>
2019-10-24 13:38:13 +0300

    lesson3

---
+++ lesson3/web/scripts.js
@@ -0,0 +1,15 @@
+console.log ("Hello");
+
+
+//let x=10;
+//let z=x+y
+// alert (x) // запрос пользователю
+
+х=promt("enter x");// текст
+
+
+ х=parseInt(promt("enter x"));
+
+function foo() {
+	alert ("You click the button")
+}
